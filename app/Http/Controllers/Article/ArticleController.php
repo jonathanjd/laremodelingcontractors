98c9982e881698023cfe9a3cb9aae6b4d24d95f5
Jonathan Duran <headjd@gmail.com>
2018-06-13 17:41:12 -0400

    Last Fix

--- app/Http/Controllers/Article/ArticleController.php
+++ app/Http/Controllers/Article/ArticleController.php
@@ -10,6 +10,16 @@ use App\Http\Controllers\Controller;
 
 class ArticleController extends Controller
 {
+
+    /**
+     * __construct
+     * Middleare auth
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
