5387da1eae3be41bbe7c855c2b09518a22cf697c
Jonathan Duran <headjd@gmail.com>
2018-06-12 20:43:25 -0400

    Modelo Setup Article

---
+++ app/Article.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    //
+    protected $fillable = [
+        'title',
+        'permalink',
+        'body',
+        'img_url',
+        'img_name',
+        'seo_title',
+        'seo_description',
+        'keywords',
+        'status',
+        'visibility',
+        'user_id',
+        'deleted_by'
+    ];
+}
